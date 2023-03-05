33dbcc83bff5d2ed07219fb7c3b92752f1a4311a
LAPTOP-OJ757MOJ\Sofija <sofija.shishevska@learnlight.com>
2023-03-05 12:36:04 +0100

    tests for validation logic and resources controller added

---
+++ ProOffice.Tests/ResourceAPI.Tests/Controllers/ResourceApiControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using ProOffice.ResourceAPI.Controllers;
+using ProOffice.ResourceAPI.Models.Dto;
+using ProOffice.ResourceAPI.Repository;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ProOffice.Tests.ResourceAPI.Tests.Controllers
+{
+    public class ResourceApiControllerTests
+    {
+        private IResourceRepository _resourceRepository;
+        protected ResponseDto _response;
+
+        public ResourceApiControllerTests()
+        {
+            _resourceRepository = A.Fake<IResourceRepository>();
+            _response = new ResponseDto();
+        }
+
+        [Fact]
+        public void ShouldReturnAllResources()
+        {
+            //Arrange
+            var resources = A.Fake<IEnumerable<ResourceDto>>();
+            A.CallTo(() => _resourceRepository.GetResources()).Returns(resources);
+            var controller = new ResourceApiController(_resourceRepository);
+
+            //Act
+            var result = controller.Get();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<ResponseDto>();
+        }
+    }
+}
---
+++ ProOffice.Tests/ValidationLogic.Tests/ValidateTests.cs
@@ -0,0 +1,129 @@
+﻿using ProOffice.ValidationLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProOffice.Tests.ValidationLogic
+{
+    public class ValidateTests
+    {
+        [Fact]
+        public void ShouldReturnTrueIfThereArentBookedTimeSlots()
+        {
+            // Arrange
+            var requestedStartDate = new DateTime(2023,05,09);
+            var requestedEndDate = new DateTime(2023,06,09);
+            var alreadyBookedTimeSlots = new List<TimeSlot>();
+            Validate validate = new();
+
+            // Act
+            bool isTimeRangeAvailable = validate.isRequestedTimeRangeAvailable(requestedStartDate, requestedEndDate, alreadyBookedTimeSlots);
+
+            // Assert
+            Assert.True(isTimeRangeAvailable);
+        }
+
+        [Fact]
+        public void ShouldReturnTrueIfRequestedTimeSlotIsNotOverlapping()
+        {
+            // Arrange
+            var requestedStartDate = new DateTime(2023, 05, 07);
+            var requestedEndDate = new DateTime(2023, 05, 15);
+            var alreadyBookedTimeSlots = new List<TimeSlot>
+            {
+                new TimeSlot { DateFrom = new DateTime(2023, 06, 09),  DateTo = new DateTime(2023, 06, 10), }
+            };
+
+            Validate validate = new();
+
+            // Act
+            bool isTimeRangeAvailable = validate.isRequestedTimeRangeAvailable(requestedStartDate, requestedEndDate, alreadyBookedTimeSlots);
+
+            // Assert
+            Assert.True(isTimeRangeAvailable);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseIfRequestedStartDateIsAtBookedTimeSlot()
+        {
+            // Arrange
+            var requestedStartDate = new DateTime(2023, 05, 11);
+            var requestedEndDate = new DateTime(2023, 06, 15);
+            var alreadyBookedTimeSlots = new List<TimeSlot>
+            {
+                new TimeSlot { DateFrom = new DateTime(2023, 05, 10),  DateTo = new DateTime(2023, 06, 10), }
+            };
+
+            Validate validate = new();
+
+            // Act
+            bool isTimeRangeAvailable = validate.isRequestedTimeRangeAvailable(requestedStartDate, requestedEndDate, alreadyBookedTimeSlots);
+
+            // Assert
+            Assert.False(isTimeRangeAvailable);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseIfRequestedTimeSlotIsNotValid()
+        {
+            // Arrange
+            var requestedStartDate = new DateTime(2023, 05, 07);
+            var requestedEndDate = new DateTime(2023, 05, 06);
+            var alreadyBookedTimeSlots = new List<TimeSlot>
+            {
+                new TimeSlot { DateFrom = new DateTime(2023, 06, 09),  DateTo = new DateTime(2023, 06, 10), }
+            };
+
+            Validate validate = new();
+
+            // Act
+            bool isTimeRangeAvailable = validate.isRequestedTimeRangeAvailable(requestedStartDate, requestedEndDate, alreadyBookedTimeSlots);
+
+            // Assert
+            Assert.False(isTimeRangeAvailable);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseIfRequestedEndDateIsAtBookedTimeSlot()
+        {
+            // Arrange
+            var requestedStartDate = new DateTime(2023, 05, 07);
+            var requestedEndDate = new DateTime(2023, 05, 15);
+            var alreadyBookedTimeSlots = new List<TimeSlot>
+            {
+                new TimeSlot { DateFrom = new DateTime(2023, 05, 10),  DateTo = new DateTime(2023, 06, 10), }
+            };
+
+            Validate validate = new();
+
+            // Act
+            bool isTimeRangeAvailable = validate.isRequestedTimeRangeAvailable(requestedStartDate, requestedEndDate, alreadyBookedTimeSlots);
+
+            // Assert
+            Assert.False(isTimeRangeAvailable);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseIfBookedTimeSlotIsBitweenRequestedTimeSlot()
+        {
+            // Arrange
+            var requestedStartDate = new DateTime(2023, 05, 07);
+            var requestedEndDate = new DateTime(2023, 05, 15);
+            var alreadyBookedTimeSlots = new List<TimeSlot>
+            {
+                new TimeSlot { DateFrom = new DateTime(2023, 05, 09),  DateTo = new DateTime(2023, 05, 10), }
+            };
+
+            Validate validate = new();
+
+            // Act
+            bool isTimeRangeAvailable = validate.isRequestedTimeRangeAvailable(requestedStartDate, requestedEndDate, alreadyBookedTimeSlots);
+
+            // Assert
+            Assert.False(isTimeRangeAvailable);
+        }
+    }
+}
